caa0ece23645e4581f4f67a31f0cc1d6a8ce6d27
Damien Ouzillou <ouzillou.dam@gmail.com>
2024-07-26 11:05:44 +0200

    Feature/game functionality beginning of the game (#1)
    * beginning of the game
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * adding test

--- game2048.py
+++ game2048.py
@@ -1,5 +1,19 @@
+from random import randint
+
 import numpy as np
 
-temp = np.eye(5)
+# On initialise le jeu
+score = 0
+game_matrix = np.empty((4, 4))
+init_row_1, init_column_1 = randint(0, len(game_matrix) - 1), randint(0, len(game_matrix) - 1)
+init_row_2, init_column_2 = randint(0, len(game_matrix) - 1), randint(0, len(game_matrix) - 1)
+
+# On initialise deux 2 qui sont à des coordonnées distinctes
+while (init_row_2 == init_row_1) and (init_column_2 == init_column_1):
+    init_row_2, init_column_2 = randint(0, len(game_matrix)), randint(0, len(game_matrix) - 1)
+
+game_matrix[init_row_1, init_column_1] = 2
+game_matrix[init_row_2, init_column_2] = 2
+
+print("test")
 print("hello")
-print(temp)

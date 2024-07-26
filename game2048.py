c92aca2ffcb8acd627bb416a417dd79727121ec9
DamsOuz <ouzillou.dam@gmail.com>
2024-07-26 10:54:33 +0200

    test

--- game2048.py
+++ game2048.py
@@ -5,7 +5,7 @@ import numpy as np
 # On initialise le jeu
 score = 0
 game_matrix = np.empty((4, 4))
-init_row_1, init_column_1 = randint(0, len(game_matrix) - 1), randint(0, len(game_matrix) - 1) + 2033484839032 + 3049203204
+init_row_1, init_column_1 = randint(0, len(game_matrix) - 1), randint(0, len(game_matrix) - 1)
 init_row_2, init_column_2 = randint(0, len(game_matrix) - 1), randint(0, len(game_matrix) - 1)
 
 # On initialise deux 2 qui sont à des coordonnées distinctes
